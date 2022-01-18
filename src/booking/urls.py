6e8aa9e1ca267bce664c64a78a04883c791cd121
eduoblys <eduoblys@gmail.com>
2022-01-18 19:13:23 +0100

    fixed Reservation page forms

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='booking-home'),
     path('reservation/', views.showform, name='reservation-form'),
+    
 
 ]
--- src/bookingapp/settings.py
+++ src/bookingapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'crispy_forms',
+    'django_tables2',
     'debug_toolbar',
 
 ]
